999fa3a3eb202cbf775ae7e27fdbc666f588de75
Khushbu2314 <114127070+Khushbu2314@users.noreply.github.com>
2022-11-07 10:06:08 -0800

    Add files via upload

---
+++ Survival_analysis.r
@@ -0,0 +1,34 @@
+#input1-log2cpm ....take subset of genes 
+#input2-,vector or array of genes 
+#take the z_scores
+#function format
+#plot heat map
+#cgga 
+library(survival)
+library(survminer)
+setwd("C:\\Users\\sai\\Documents\\R")
+data = read.table('CGGA.mRNAseq_693_clinical.20200506.txt',
+                  header = T, sep = '\t', row.names = 1)
+
+fit =  survfit(Surv(OS, Censor..alive.0..dead.1.)  ~  Gender, 
+               data = data)
+fit
+
+
+ggsurvplot(fit, data = data)
+
+ggsurvplot(fit, data = data,
+           surv.median.line = 'hv')
+
+ggsurvplot(fit, data = data,
+           surv.median.line = 'hv',
+           pval = T)
+
+ggsurvplot(fit, data = data,
+           surv.median.line = 'hv',
+           pval = T, risk.table = T)
+fit =  survfit(Surv(OS, Censor..alive.0..dead.1.) ~ IDH_mutation_status, 
+               data = data)
+
+fit =  survfit(Surv(OS, Censor..alive.0..dead.1.) ~ IDH_mutation_status + Gender, 
+               data = data)
